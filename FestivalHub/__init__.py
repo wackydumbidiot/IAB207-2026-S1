266508aa74827c3758a24d4d24beddf474ddc485
HenryN12 <236012331+HenryN-bit@users.noreply.github.com>
2026-05-08 17:56:38 +1000

    fix: fixed flask

---
+++ FestivalHub/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+def create_app():
+  
+    app = Flask(__name__)  # this is the name of the module/package that is calling this app
+
+    @app.route("/")
+    # Should be set to false in a production environment
+
+    def index():
+        return "Flask is running"
+    
+    # app.debug = True
+
+    return app
+    # app.secret_key = 'somesecretkey'
+    # set the app configuration data 
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
+    # initialise db with flask app
+
+    # Bootstrap5(app)
--- auth.py
+++ auth.py
@@ -8,18 +8,18 @@ db = SQLAlchemy()
 
 # create a function that creates a web application
 # a web server will run this web application
-def create_app():
+def loginManager():
   
-    app = Flask(__name__)  # this is the name of the module/package that is calling this app
-    # Should be set to false in a production environment
-    app.debug = True
-    app.secret_key = 'somesecretkey'
-    # set the app configuration data 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
-    # initialise db with flask app
-    db.init_app(app)
+   #  app = Flask(__name__)  # this is the name of the module/package that is calling this app
+   #  # Should be set to false in a production environment
+   #  app.debug = True
+   #  app.secret_key = 'somesecretkey'
+   #  # set the app configuration data 
+   #  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
+   #  # initialise db with flask app
+   #  db.init_app(app)
 
-    Bootstrap5(app)
+   #  Bootstrap5(app)
     
     # initialise the login manager
     login_manager = LoginManager()

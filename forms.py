37e003fe46a6b04e521cf136eeed28237bda0c6c
vijeet06-debug <vijeet06@gmail.com>
2026-05-12 13:33:33 +1200

    Refactor LoginForm by removing unnecessary lines

--- forms.py
+++ forms.py
@@ -4,16 +4,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo, Length
 
 class LoginForm(FlaskForm):
-    
-    user_name = StringField(
-        'Username',
-        validators=[DataRequired()]
-    )
-    
-    password = PasswordField(
-        'Password',
-        validators=[DataRequired()]
-    )
+    user_name = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('login')
                          
 

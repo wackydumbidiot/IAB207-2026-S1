f5b1da32aa5c271433322ae38af11dc969bfa103
vijeet06-debug <vijeet06@gmail.com>
2026-05-19 02:36:20 +1200

    Create database

--- models.py
+++ models.py
@@ -1,21 +1,47 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from . import db
+from datetime import datetime
+from flask_login import UserMixin
 
-# creates the login information
-class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
-    submit = SubmitField("Login")
 
- # this is the registration form
-class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    # linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
 
-    # submit button
-    submit = SubmitField("Register")
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    orders = db.relationship('Order', backref='user', lazy=True)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    description = db.Column(db.Text, nullable=False)
+
+    date = db.Column(db.DateTime, nullable=False)
+
+    location = db.Column(db.String(100), nullable=False)
+
+    capacity = db.Column(db.Integer, nullable=False)
+
+    orders = db.relationship('Order', backref='event', lazy=True)
+
+
+class booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    booking_id = db.Column(db.String(20), unique=True, nullable=False)
+
+    booking_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    quantity = db.Column(db.Integer, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+
+

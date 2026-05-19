455c6cbcee506fd34153237097fd6874df5ac9c7
vijeet06-debug <vijeet06@gmail.com>
2026-05-19 12:52:02 +1200

    Refactor forms.py to include registration and booking forms
    Added user registration and booking forms alongside the login form.

--- forms.py
+++ forms.py
@@ -1,11 +1,25 @@
-#User Login Form
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import InputRequired, Email, EqualTo
 
+#user login form
 class LoginForm(FlaskForm):
-    user_name = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('login')
-                         
+    user_name = StringField("User Name", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
 
+#user registration form
+class RegisterForm(FlaskForm):
+    user_name = StringField("User Name", validators=[InputRequired()])
+    email = StringField("Email Address", validators=[Email()])
+    password = PasswordField("Password", validators=[
+        InputRequired(),
+        EqualTo('confirm', message="Passwords should match")
+    ])
+    confirm = PasswordField("Confirm Password")
+    submit = SubmitField("Register")
+
+#booking form
+class BookingForm(FlaskForm):
+    quantity = IntegerField("Number of Tickets", validators=[InputRequired()])
+    submit = SubmitField("Confirm Booking")

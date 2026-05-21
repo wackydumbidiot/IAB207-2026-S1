c9e34861869f38043ff3f87a670f90069073bc0e
vijeet06-debug <vijeet06@gmail.com>
2026-05-21 23:23:23 +1200

    Order Database
    Added Order class for booking management.

--- FestivalHub/models.py
+++ FestivalHub/models.py
@@ -29,3 +29,12 @@ class Event(db.Model):
 
 # class Order(db.Model):
 #     pass
+# Booking/order database table
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.String(20), unique=True, nullable=False)
+    booking_date = db.Column(db.DateTime, default=datetime.utcnow)
+    quantity = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)

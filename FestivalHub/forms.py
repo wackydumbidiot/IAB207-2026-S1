6d97b9cef64f9abe7651523d53de50090c071a86
Stanley <stanleycai111@gmail.com>
2026-05-25 16:42:23 +1000

    Revert "Add event comments and demo data"
    This reverts commit 374d805f689e19d1aef1274e449ce0897c62df49.

--- FestivalHub/forms.py
+++ FestivalHub/forms.py
@@ -10,28 +10,18 @@ from wtforms.validators import InputRequired, Length, Email, EqualTo, NumberRang
 
 # creates the login information
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name'),
-                                                Email("Please enter a valid email")])
+    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
     password=PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
  # this is the registration form
 class RegisterForm(FlaskForm):
-    first_name = StringField("First Name", validators=[InputRequired("Enter your first name")])
-    surname = StringField("Surname", validators=[InputRequired("Enter your surname")])
-    email = StringField("Email Address", validators=[
-        InputRequired("Enter your email"),
-        Email("Please enter a valid email")
-    ])
+    user_name=StringField("User Name", validators=[InputRequired()])
+    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
     # linking two fields - password should be equal to data entered in confirm
-    password = PasswordField("Password", validators=[
-        InputRequired("Enter your password"),
-        EqualTo('confirm', message="Passwords should match")
-    ])
-    confirm = PasswordField("Confirm Password", validators=[InputRequired("Confirm your password")])
-    contact_number = StringField("Contact Number", validators=[InputRequired("Enter your contact number number")])
-    street_address = StringField("Street Address", validators=[InputRequired("Enter your street address")])
-
+    password=PasswordField("Password", validators=[InputRequired(),
+                  EqualTo('confirm', message="Passwords should match")])
+    confirm = PasswordField("Confirm Password")
 
     # submit button
     submit = SubmitField("Register")
@@ -86,13 +76,3 @@ class CreateOrUpdateEventForm(FlaskForm):
     event_image = FileField("Event Image:", validators=[InputRequired(message="Please upload an event image.")])
 
     submit = SubmitField("Create Event", validators=[InputRequired()])
-
-
-
-
-class CommentForm(FlaskForm):
-    text = TextAreaField("Comment", validators=[
-        InputRequired("Commenting cannot be empty."),
-        Length(max=500, message="Comment not exceeding more than 500 characters.")
-    ])
-    submit = SubmitField("Post Comment")
--- FestivalHub/models.py
+++ FestivalHub/models.py
@@ -2,23 +2,8 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-
-    first_name = db.Column(db.String(100), nullable=False)
-    surname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    password_hash = db.Column(db.String(255), nullable=False)
-    contact_number = db.Column(db.String(20), nullable=False)
-    street_address = db.Column(db.String(255), nullable=False)
-
-    created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
-    comments = db.relationship('Comment', backref='user', lazy=True)
-     # String print method
-    def __repr__(self):
-        return f"Username: {self.username}, Email: {self.email}"    
-
+# class User(db.Model, UserMixin):
+#     pass
 
 class Event(db.Model):
     __tablename__ = 'events'
@@ -37,23 +22,12 @@ class Event(db.Model):
     image = db.Column(db.String(400), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
 
-    comments = db.relationship('Comment', backref='event', lazy=True)
     # String print method
     def __repr__(self):
         return f"Name: {self.name}"
 
-class Comment(db.Model):
-    __tablename__ = 'comments'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
-    comment_text = db.Column(db.String(500), nullable=False)
-    created_at = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
-
-
+# class Comment(db.Model):
+#     pass
 
 # class Order(db.Model):
 #     pass

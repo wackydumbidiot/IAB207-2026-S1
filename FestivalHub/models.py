36cbbe33fd8565d0565037aeb02195e6454baef6
vijeet06-debug <vijeet06@gmail.com>
2026-06-11 15:31:11 +1000

    Fix Order foreign key reference

--- FestivalHub/models.py
+++ FestivalHub/models.py
@@ -67,7 +67,7 @@ class Order(db.Model):
     order_id = db.Column(db.String(20), unique=True, nullable=False)
     booking_date = db.Column(db.DateTime, default=datetime.utcnow)
     quantity = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
 
     event = db.relationship('Event', backref='orders')
